f424bf2ac71e6328d49c5e3bc47b6b614c7452d7
Eric <Ericrius1@gmail.com>
2014-08-15 16:46:27 +0200

    sound

--- js/magicball.js
+++ js/magicball.js
@@ -1,17 +1,41 @@
 function MagicBall(){
+  var audio = AUDIO['omm'];
   var ballGeo = new THREE.IcosahedronGeometry(50, 2);
+  var mat = new THREE.ShaderMaterial({
+    uniforms:{
+      timer: timer,
+      t_normal: {type: 't', value: TEXTURES.moss},
+      cameraPos: {type: 'v3', value: camera.position},
+      color: {type:'v3', value: new THREE.Vector3(0.7, 0.1, 0.7)},
+      t_audio: {type: 't', value: audio.texture},
+    }
+  })
   var ball = new THREE.Mesh(ballGeo);
+  ball.playing = false;
+  ball.audio = audio;
   ball.position.y = 200;
   scene.add(ball);
 
+  looper.everyLoop(function(){this.audio.play}.bind(ball));
+
   objectControls.add(ball);
 
-  var audio = AUDIO['omm'];
 
   ball.select = function(){
+    this.playing = !this.playing;
+    if(this.playing === false){
+      this.audio.gain.gain.value = 0.0;
+    }else{
+      this.audio.gain.gain.value = 0.8;
+    }
     ball.material.color.r = 1;
     audio.play();
 
+  }.bind(ball);
+
+  ball.deselect = function(){
+    this.playing = false;
+    this.audio.gain.gain.value = 0.0;
   }
 
 
--- js/main.js
+++ js/main.js
@@ -6,50 +6,61 @@
 
 var scene, camera, renderer, clock, objectControls;
 var pond, petalObject, pond, magicball;
-var timer = {type: 'f', value: 0};
+var timer = {
+  type: 'f',
+  value: 0
+};
 var loader = new Loader();
-loader.onStart = function(){
+loader.onStart = function() {
   init();
   animate();
 }
 loader.beginLoading();
-new THREE.OBJMTLLoader().load('assets/petal.obj', 'assets/petal.mtl', function(object){
+new THREE.OBJMTLLoader().load('assets/petal.obj', 'assets/petal.mtl', function(object) {
   petalObject = object;
   loader.endLoading()
 })
 var TEXTURES = {};
 var normals = [];
 loadTexture('moss', 'assets/normals/sand.png', normals)
-  
+
 var shaders = new ShaderLoader('shaders');
 
 loader.beginLoading();
 shaders.load('vs-pond', 'pond', 'vertex');
 shaders.load('fs-pond', 'pond', 'fragment');
 
-shaders.shaderSetLoaded = function(){
+shaders.shaderSetLoaded = function() {
   loader.endLoading();
 }
 
-var gui = new dat.GUI({autoplace: false});
+var gui = new dat.GUI({
+  autoplace: false
+});
 gui.close();
 
 var guiContainer = document.getElementById('GUI');
 guiContainer.appendChild(gui.domElement)
 
 
+
+var looper = new Looper(audioController, timer, {
+
+  beatsPerMinute: 120,
+  beatsPerMeasure: 4,
+  measuresPerLoop: 8
+
+});
+
+
 //*****************AUDIO***************
 var audioController = new AudioController();
 var AUDIO = {};
 loader.beginLoading();
-loader.beginLoading();
 var file = '';
-AUDIO['splendor'] = new LoadedAudio(audioController, 'audio/allRight.mp3');
 AUDIO['omm'] = new LoadedAudio(audioController, 'audio/omm.mp3');
-AUDIO['splendor'].onLoad = function(){
-  loader.endLoading();
-}
-AUDIO['omm'].onLoad = function(){
+
+AUDIO['omm'].onLoad = function() {
   loader.endLoading();
 }
 
@@ -65,7 +76,7 @@ function init() {
   renderer = new THREE.WebGLRenderer({
     antialias: true
   });
-  camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 200000);
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 200000);
   camera.position.z = 2000;
   renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -73,9 +84,9 @@ function init() {
   container.id = "container";
   document.body.appendChild(container);
   container.appendChild(renderer.domElement);
-  
+
   controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.maxPolarAngle = Math.PI/2.1;
+  controls.maxPolarAngle = Math.PI / 2.1;
 
   objectControls = new ObjectControls(camera);
 
@@ -96,14 +107,14 @@ function animate() {
 }
 
 function onResize() {
-  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function loadTexture(name, file, array){
+function loadTexture(name, file, array) {
   this.loader.beginLoading();
-  var cb = function(){
+  var cb = function() {
     loader.endLoading();
   };
 
